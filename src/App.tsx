640b1bf5507c3e4420f017025f19e52d21063c7a
Wender Barbosa <wenblack012@gmail.com>
2022-08-06 17:12:15 -0300

    Translate App Page to Portuguese

--- src/App.tsx
+++ src/App.tsx
@@ -18,15 +18,15 @@ function App() {
       <h1>Página de Exemplo</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          O contador é  {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edite o arquivo <code>src/App.tsx</code> e salve para testar
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        Clique nos logos do Vite e do React para aprender mais
+        </p>
     </div>
   )
 }
